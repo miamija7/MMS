5c0a8396d285851e99cb1af9dfa2a17d3408e6b5
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-10-08 13:05:28 -0500

    Contact Us Modal

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ setTimeout(timeout, 3000);
 document.addEventListener('DOMContentLoaded', () => {
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    const $navbarButtons = document.querySelector('#contact-us');
     // Add a click event on each of them
     $navbarBurgers.forEach( el => {
         el.addEventListener('click', () => {
@@ -19,6 +20,14 @@ document.addEventListener('DOMContentLoaded', () => {
             el.classList.toggle('is-active');
             $target.classList.toggle('is-active');
         });
+        $navbarButtons.addEventListener('click', () => {
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+        })
     });
 });
 
